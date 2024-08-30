86d793a4730b12ff3d6c2d951997dcd3d123b725
noah.chang <yichen8625@gmail.com>
2024-08-30 14:46:42 +0800

    Finish Case8、Case9、Case10

---
+++ Test Suites/Case8 管理員完成寄放物品登記.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>管理員完成寄放物品登記</description>
+   <name>Case8 管理員完成寄放物品登記</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>20</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>946eb50f-1787-45e4-9489-40edb24d66b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>f86c9bc4-69e0-43e2-b59f-b83fc6c7ebef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case8 (Common)/housekeeper login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a88cf31a-1329-4813-b165-26646af2a4a9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a54bb7f4-bd75-458b-a5c1-5952ebcb32ff</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b8f80a2-3899-4748-bd10-12bd3235c98d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case8 (Common)/register the things</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>e97176eb-b259-4c00-97ff-20a7c4c413b0</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
